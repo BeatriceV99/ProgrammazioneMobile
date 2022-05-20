1340bb5240e204a79a7e069c5bd47448e5cd3478
BeatriceV99 <101866682+BeatriceV99@users.noreply.github.com>
2022-05-20 16:08:59 +0200

    Add files via upload

---
+++ Programma/app/src/main/java/com/experiments/progrmob/fragments/DayListFragment.kt
@@ -0,0 +1,80 @@
+package com.experiments.progrmob.fragments
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListAdapter
+import android.widget.ListView
+import com.experiments.progrmob.R
+import com.experiments.progrmob.models.MyArrayAdapter
+import com.experiments.progrmob.models.MyEvent
+import com.experiments.progrmob.models.getEventsOfDay
+import com.experiments.progrmob.models.mergeMyEventWithFirebaseInfo
+import kotlinx.coroutines.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DayListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DayListFragment : Fragment() {
+    private var day: Int? = null
+    private var month: Int? = null
+    private var year: Int? = null
+    // private var events : List<MyEvent>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            day = it.getInt("day")
+            month = it.getInt("month")
+            year = it.getInt("year")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_day_list, container, false)
+
+        val context : Context = this.requireContext()
+        val eventList : ListView = view.findViewById(R.id.eventList)
+
+        // See: https://developer.android.com/kotlin/coroutines/coroutines-adv#main-safety
+        // See: https://stackoverflow.com/questions/53079234/how-to-update-ui-in-coroutines-in-kotlin-1-3
+        CoroutineScope(Dispatchers.Main + Job()).launch {
+            withContext(Dispatchers.IO) {
+                val events =
+                    getEventsOfDay(requireActivity().contentResolver, day!!, month!!, year!!)
+
+                mergeMyEventWithFirebaseInfo(context, events)
+
+                // To update the UI, set the Main context
+                withContext(Dispatchers.Main) {
+                    val adapter: ListAdapter = MyArrayAdapter(requireActivity(), 0, events)
+                    eventList.setAdapter(adapter as ListAdapter)
+                }
+            }
+        }
+
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(/*events : List<MyEvent>*/day : Int, month: Int, year: Int) =
+            DayListFragment().apply {
+                arguments = Bundle().apply {
+                    putInt("day", day)
+                    putInt("month", month)
+                    putInt("year", year)
+                }
+            }
+    }
+}
---
+++ Programma/app/src/main/java/com/experiments/progrmob/models/MyEvent.kt
@@ -0,0 +1,28 @@
+package com.experiments.progrmob.models
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+// See: https://github.com/kizitonwose/CalendarView
+class MyEvent (val id : Long,
+               val calId : Long,
+               val title: String,
+               val descr : String,
+               val startDate : Date,
+               val endDate: Date,
+               val duration: String,
+               val allDay: Boolean,
+               var isWeather: Boolean = false,
+               var location: String = "") {
+    private val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US)
+
+    val formattedStartDate : String?
+        get() : String? {
+            return formatter.format(this.startDate)
+        }
+
+    val formattedEndDate : String?
+        get() : String? {
+            return formatter.format(this.endDate)
+        }
+}
